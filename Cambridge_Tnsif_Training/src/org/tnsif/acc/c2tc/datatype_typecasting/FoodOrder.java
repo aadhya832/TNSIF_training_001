001b5b9dcffd51999bb1d449decae437d2b0ba7d
Aadhya M <Aadhya M@Aadhya>
2026-08-10 19:38:14 +0530

    first commit

---
+++ Cambridge_Tnsif_Training/src/org/tnsif/acc/c2tc/datatype_typecasting/FoodOrder.java
@@ -0,0 +1,18 @@
+package org.tnsif.acc.c2tc.datatype_typecasting;
+
+public class FoodOrder {
+	public static void main(String[]args) {
+		int ordered=101;
+		double price=299.99;
+		float isDelivered=5.3f;
+		long deliveryBoyPhone=9876453527L;
+		byte deliverTime=45;
+		short restaurantld=12;
+		
+		String CustomerName="Anjali";
+		String[]foodItem= {"Burger","Fries","Coke"};
+		
+		
+	}
+
+}
