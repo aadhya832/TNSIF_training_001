804e69c7297146fb4d06f7692907f2bc32ad63ea
Aadhya M <Aadhya M@Aadhya>
2026-08-17 19:53:37 +0530

    first commit

---
+++ Cambridge_Tnsif_Training/src/org/tnsif/acc/c2tc/interviewquestions/Palindrome.java
@@ -0,0 +1,22 @@
+package org.tnsif.acc.c2tc.interviewquestions;
+import java.util.Scanner;
+public class Palindrome {
+	public static void main(String[]args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the Number");
+		int num=sc.nextInt();
+		int original=num;
+		int reverse=0;
+		while(num!=0) {
+			int digit = num%10;
+			reverse=reverse*10+digit;
+			num=num/10;
+		}
+		if(original==reverse) {
+			System.out.println("the number is palindrome");
+		}else {
+			System.out.println("the number is not palindrome");
+		}
+	}
+
+}
---
+++ Cambridge_Tnsif_Training/src/org/tnsif/acc/c2tc/interviewquestions/ReverseString.java
@@ -0,0 +1,18 @@
+package org.tnsif.acc.c2tc.interviewquestions;
+import java.util.Scanner;
+public class ReverseString {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter  STRING");
+		String str=sc.nextLine();
+		String reverse=" ";
+		for(int i =str.length()-1;i>=0;i--) {
+			reverse=reverse+str.charAt(i);
+			System.out.println("Reversed string:"+reverse);
+			sc.close();
+		}
+		
+	}
+
+}
